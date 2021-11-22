425deeaf795a880abe98c76c974afd07e3fdae41
Alexander Mironov <alexan9610@gmail.com>
2021-11-22 14:18:28 +0300

    Реализация функции edit(), задания: 7-11 (так же: доработка кода предыдущих заданий)

--- Module-6/telegraph-main.php
+++ Module-6/telegraph-main.php
@@ -1,40 +1,78 @@
 <?php
 
-/* Инициализация переменных*/
+/* Инициализация переменных */
 $textStorage = []; // основной массив задания
 
 /**
- * Функция производит заполнение глобального массива переданными параметрами в виде ассоциативного массива
+ * Функция производит заполнение глобального массива globalArr[] переданными параметрами
+ * в виде ассоциативного массива ['title' => 'title', 'text' => 'text']
+ * @param array $globalArr принимаемый по адресу глобальный массив
  * @param string $title Принимаемый параметр заголовка, присвоится ключу 'title'
  * @param string $text Принимаемый параметр текста, присвоится ключу 'text'
  */
-function add(string $title = '', string $text = '') : void
+function add(array &$globalArr, string $title = 'default title', string $text = 'default text') : void
 {
-    global $textStorage;
-    $textStorage[] = ['title' => $title, 'text' => $text];
+    $globalArr[] = ['title' => trim($title), 'text' => trim($text)];
 }
 
 /**
  * Функция производит удаление из глобального массива конкретного элемента по переданному индексу
+ * @param array $globalArr принимаемый по адресу глобальный массив
  * @param null $key  передаваемый индекс
  * @return bool Возвращает false если индекс не задан, или в массиве нет значения по индексу, в ином случае - true
  */
-function remove($key = null) : bool
+function remove(array &$globalArr, $key = null) : bool
 {
-    global $textStorage;
-    if (isset($key, $textStorage[$key]) === false) {
+    if (isset($key, $globalArr[$key]) === false) {
         return false;
     }
-    array_splice($textStorage, $key, 1);
+    array_splice($globalArr, $key, 1);
     return true;
 }
 
-add('main', 'hello');
-add('my name', "i will not tell you my name" );
+/**
+ * Функция редактирует title||text ассоциативного массива по заданному индексу в передаваемом глобальном массиве
+ * @param array $globalArr принимаемый по адресу глобальный массив
+ * @param null $key передаваемый индекс
+ * @param string $newTitle Новый заголовок
+ * @param string $newText Новый текст
+ * @return bool Возвращает false если индекс не задан, или в массиве нет значения по индексу, в ином случае - true
+ */
+function edit(array &$globalArr, $key = null, string $newTitle = '', string $newText = '') : bool
+{
+    if (isset($key, $globalArr[$key]) === false) {
+        return false;
+    }
+    /* Если newTitle определен и не пустой - перезаписывает 'title' */
+    if (trim($newTitle) !== '') {
+        $globalArr[$key]['title'] = trim($newTitle);
+    }
+    /* Если newText определен и не пустой - перезаписывает 'text' */
+    if (trim($newText) !== '') {
+        $globalArr[$key]['text'] = trim($newText);
+    }
+    return true;
+}
 
+/* Testing code */
+
+// Задание 3
+add($textStorage, 'main', 'hello');
+add($textStorage, 'my name', "i will not tell you my name");
 print_r($textStorage);
 
-var_dump(remove(0));
-var_dump(remove(5));
+// Задание 5
+var_dump(remove($textStorage,0));
+var_dump(remove($textStorage, 5));
 
+// Задание 6
 print_r($textStorage);
+
+// Задание 8
+edit($textStorage, 0, '   Name of student  ');
+
+// Задание 9
+var_dump($textStorage[0]);
+
+// Задание 10
+var_dump(edit($textStorage, 66));
