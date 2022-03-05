17b5d6e766bee6909c505db72ced332ebbfd4514
Alexander Mironov <alexan9610@gmail.com>
2022-03-05 22:53:43 +0300

    Добавление ассетов и шаблонов html

---
+++ Module-15/app/templates/footer.php
@@ -0,0 +1,29 @@
+<?php if (! defined('PROJECT_CORE') || PROJECT_CORE !== true) {die;} ?>
+    </main>
+
+    <footer class="flex-block footer">
+        <div class="content-between-position">
+            <ul class="footer-menu list">
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+            </ul>
+            <div>
+                <p>test@mail.ru</p>
+                <a href="tel:+79999999999" class="link phone">+7 (999) 999-99-99</a>
+            </div>
+            <div class="links-block">
+                <button class="button-tranperent button on-blue">Test</button>
+                <ul class="link-list list">
+                    <li><a href="https://www.facebook.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_PATH?>/app/assets/images/facebook.png" alt="this pic"></a></li>
+                    <li><a href="https://vk.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_PATH?>/app/assets/images/vk.png" alt="this pic"></a></li>
+                    <li><a href="https://web.telegram.org" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_PATH?>/app/assets/images/telegram.png" alt="this pic"></a></li>
+                    <li><a href="https://www.youtube.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_PATH?>/app/assets/images/youtube.png" alt="this pic"></a></li>
+                    <li><a href="https://www.instagram.com/" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_PATH?>/app/assets/images/instagram.png" alt="this pic"></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
---
+++ Module-15/app/templates/header.php
@@ -0,0 +1,29 @@
+<?php if (! defined('PROJECT_CORE') || PROJECT_CORE !== true) {die;} ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=PROJECT_PATH?>/app/assets/css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header class="flex-block header">
+        <div class="content-between-position">
+            <ul class="header-menu list">
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+            </ul>
+            <div class="header-feedback">
+                <a href="tel:+79999999999" class="link phone">+7 (999) 999-99-99</a>
+                <button class="button-tranperent button on-blue">Test</button>
+            </div>
+        </div>
+    </header>
+
+    <main>
