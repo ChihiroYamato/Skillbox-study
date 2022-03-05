1e3d58a52be17fc50fb190b31239cd357e4edad6
Alexander Mironov <alexan9610@gmail.com>
2022-03-05 22:49:12 +0300

    Подключение файла констант

---
+++ Module-15/app/core/const.php
@@ -0,0 +1,20 @@
+<?php
+
+if (! defined('PROJECT_CORE')) {
+    define('PROJECT_CORE', true);
+}
+if (! defined('PROJECT_PATH')) {
+    define('PROJECT_PATH', '/php-developer-base/Module-15');
+}
+if (! defined('LOGS_PATH')) {
+    define('LOGS_PATH', dirname(__DIR__) . '/logs/');
+}
+if (! defined('STORAGE_PATH')) {
+    define('STORAGE_PATH', dirname(__DIR__) . '/storage/');
+}
+if (! defined('EXCEPTION_LOGS_NAME')) {
+    define('EXCEPTION_LOGS_NAME', 'exception-logs.xml');
+}
+if (! defined('STORAGE_LOGS_NAME')) {
+    define('STORAGE_LOGS_NAME', 'file-storage-log');
+}
