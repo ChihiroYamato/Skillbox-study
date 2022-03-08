28991df544a1be167ca1e61717c5aea1abdf9b34
Alexander Mironov <alexan9610@gmail.com>
2022-03-09 00:55:16 +0300

    Доработки констант и инициирование сессии в ядре

--- Module-15/app/core/const.php
+++ Module-15/app/core/const.php
@@ -1,10 +1,17 @@
 <?php
 
+/**
+ * Константы модуля
+ */
+
 if (! defined('PROJECT_CORE')) {
     define('PROJECT_CORE', true);
 }
-if (! defined('PROJECT_PATH')) {
-    define('PROJECT_PATH', '/php-developer-base/Module-15');
+if (! defined('PROJECT_SERVER_PATH')) {
+    define('PROJECT_SERVER_PATH', '/php-developer-base/Module-15');
+}
+if (! defined('PROJECT_LOCAL_PATH')) {
+    define('PROJECT_LOCAL_PATH', dirname(__DIR__, 2));
 }
 if (! defined('LOGS_PATH')) {
     define('LOGS_PATH', dirname(__DIR__) . '/logs/');
--- Module-15/app/core/init.php
+++ Module-15/app/core/init.php
@@ -1,3 +1,10 @@
 <?php
 
+/** Старт сессии */
+session_start();
+
+/** Подключение автозагрузчика от Composer */
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+/** Установка пользовательского обработчика ошибок */
+set_exception_handler('App\Base\Helpers\Classes\Handler::handleNonCatchException');
