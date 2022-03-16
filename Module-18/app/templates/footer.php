593b0a6362fde1ab7ed349e99c1f55991f3f87c8
Alexander Mironov <alexan9610@gmail.com>
2022-03-16 19:44:59 +0300

    Инициализация ассетов и шаблонов

---
+++ Module-18/app/templates/footer.php
@@ -0,0 +1,32 @@
+<?php if (! defined('PROJECT_CORE') || PROJECT_CORE !== true) {die;} ?>
+    </main>
+
+    <footer class="flex-block footer">
+        <div class="content-between-position">
+            <ul class="footer-menu list">
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+            </ul>
+            <div class="links-block">
+                <a href="mailto: test@mail.ru" class="link">test@mail.ru</a>
+                <a href="tel:+79999999999" class="link phone">+7 (999) 999-99-99</a>
+            </div>
+            <div class="links-block">
+                <button class="button-tranperent button on-blue">Submit</button>
+                <ul class="link-list list">
+                    <li><a href="https://www.facebook.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/app/assets/images/facebook.png" alt="this pic"></a></li>
+                    <li><a href="https://vk.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/app/assets/images/vk.png" alt="this pic"></a></li>
+                    <li><a href="https://web.telegram.org" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/app/assets/images/telegram.png" alt="this pic"></a></li>
+                    <li><a href="https://www.youtube.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/app/assets/images/youtube.png" alt="this pic"></a></li>
+                    <li><a href="https://www.instagram.com/" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/app/assets/images/instagram.png" alt="this pic"></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="<?=PROJECT_SERVER_PATH?>/app/assets/js/jquery-3.6.0.min.js"></script>
+    <script src="<?=PROJECT_SERVER_PATH?>/app/assets/js/scripts.js"></script>
+
+</body>
+</html>
