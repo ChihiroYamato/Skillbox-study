6724f456be0420d087ad861438d3f6618426e58c
Alexander Mironov <alexan9610@gmail.com>
2022-03-17 00:34:29 +0300

    Доработка HtmlProcessor

--- Module-18/HtmlProcessor.php
+++ Module-18/HtmlProcessor.php
@@ -1,6 +1,6 @@
 <?php
 
-$response = '';
+$response = [];
 
 header('Content-Type: application/json');
 
@@ -8,14 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = json_decode(file_get_contents('php://input'), true);
 
     if (! empty($body)) {
-        $response = preg_replace('/<a.*>(.*)<\/a>/', '$1', array_shift($body));
+        $response['formatted_text'] = preg_replace('/<a.*>(.*)<\/a>/', '$1', array_shift($body));
     } else {
         http_response_code(500);
-        $response = 'Internal Server Error: Body is empty';
+        $response['error'] = 'Internal Server Error: Body is empty';
     }
 } else {
     http_response_code(400);
-    $response = 'Bad request: Needs method POST';
+    $response['error'] = 'Bad request: Needs method POST';
 }
 
 print_r(json_encode($response));
