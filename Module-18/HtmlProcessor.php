390d712c480524ec537613da1946b8265b7af2e8
Alexander Mironov <alexan9610@gmail.com>
2022-03-17 16:10:35 +0300

    Доработка регулярного выражения

--- Module-18/HtmlProcessor.php
+++ Module-18/HtmlProcessor.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $body = json_decode(file_get_contents('php://input'), true);
 
     if (! empty($body)) {
-        $response['formatted_text'] = preg_replace('/<a.*>(.*)<\/a>/', '$1', array_shift($body));
+        $response['formatted_text'] = preg_replace('/<a[^>]*>(.*?)<\/a>/s', '$1', array_shift($body));
     } else {
         http_response_code(500);
         $response['error'] = 'Internal Server Error: Body is empty';
@@ -18,4 +18,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $response['error'] = 'Bad request: Needs method POST';
 }
 
-print_r(json_encode($response));
+print_r(json_encode($response, JSON_FORCE_OBJECT));
