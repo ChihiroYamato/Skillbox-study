56ab3f60136632eb01d68d61ccbdb026ac79d28c
Alexander Mironov <alexan9610@gmail.com>
2022-03-17 18:45:54 +0300

    Доработка парсинга

--- Module-18/HtmlProcessor.php
+++ Module-18/HtmlProcessor.php
@@ -5,10 +5,13 @@ $response = [];
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $body = json_decode(file_get_contents('php://input'), true);
+    $requestBody = json_decode(file_get_contents('php://input'), true);
 
-    if (! empty($body)) {
-        $response['formatted_text'] = preg_replace('/<a[^>]*>(.*?)<\/a>/s', '$1', array_shift($body));
+    if (! empty($requestBody) && is_array($requestBody)) {
+        $requestElement = array_shift($requestBody);
+        $requestParse = str_replace(['<body>', '</body>', '</html>'], '', substr($requestElement, strrpos($requestElement, '<body>')));
+
+        $response['formatted_text'] = preg_replace('/<a[^>]*>(.*?)<\/a>/s', '$1', $requestParse);
     } else {
         http_response_code(500);
         $response['error'] = 'Internal Server Error: Body is empty';
