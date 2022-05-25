7cfc933616e8ba7c1db47b5490a2a98cdb8c2bcd
Alexander Mironov <alexan9610@gmail.com>
2022-05-26 02:46:58 +0300

    добавил хедер и футер

---
+++ Module-20/templates/footer.php
@@ -0,0 +1,29 @@
+    </main>
+
+    <footer class="flex-block footer">
+        <div class="content-between-position">
+            <ul class="footer-menu list">
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+                <li><a href="#" class="link hover-blue">Test</a></li>
+            </ul>
+            <div>
+                <p>test@mail.ru</p>
+                <a href="tel:+79999999999" class="link phone">+7 (999) 999-99-99</a>
+            </div>
+            <div class="links-block">
+                <button class="button-tranperent button on-blue">Submit</button>
+                <ul class="link-list list">
+                    <li><a href="https://vk.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/assets/images/vk.png" alt="this pic"></a></li>
+                    <li><a href="https://web.telegram.org" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/assets/images/telegram.png" alt="this pic"></a></li>
+                    <li><a href="https://www.youtube.com" class="link" target="_blank"><img class="messenger-icon" src="<?=PROJECT_SERVER_PATH?>/assets/images/youtube.png" alt="this pic"></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="<?=PROJECT_SERVER_PATH?>/assets/js/jquery-3.6.0.min.js"></script>
+    <script src="<?=PROJECT_SERVER_PATH?>/assets/js/scripts.js"></script>
+
+</body>
+</html>
